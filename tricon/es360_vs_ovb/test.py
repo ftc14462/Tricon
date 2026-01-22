5b2ce9d0c825f8617d310a526169cd906961aa6f
scott <scott@phoenix1>
2026-01-22 15:14:15 -0700

    new

---
+++ tricon/es360_vs_ovb/test.py
@@ -0,0 +1,21 @@
+'''
+Created on Jan 22, 2026
+
+@author: scott
+'''
+
+if __name__ == '__main__':
+    pass
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Define the data points for the x and y axes
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+# Plot the data
+plt.plot(xpoints, ypoints)
+
+# Display the plot
+plt.show()
